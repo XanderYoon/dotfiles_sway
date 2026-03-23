73fa8c3769931eca8ada08e9603d3d59fe92f96e
XanderYoon <alexander.ro.yoon@gmail.com>
2026-03-23 17:49:45 -0400

    qol

--- local/bin/launch-kitty.sh
+++ local/bin/launch-kitty.sh
@@ -3,4 +3,6 @@ set -euo pipefail
 
 export XCURSOR_SIZE="${XCURSOR_SIZE:-12}"
 
-exec /home/alexander-yoon/.local/kitty.app/bin/kitty "$@"
+shell_bin="$(command -v fish || command -v bash)"
+
+exec /home/alexander-yoon/.local/kitty.app/bin/kitty -o "shell=${shell_bin}" "$@"
