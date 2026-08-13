517c606dfa966d07c3cbff141c8d463b7cdce74d
Alexander Yoon <alexanderyoon@Alexanders-MacBook-Air.local>
2026-08-12 21:08:52 -0400

    Update fish macOS paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,10 @@
-fish_add_path --move --prepend $HOME/.local/bin
+if test -d /opt/homebrew/bin
+    fish_add_path --global --move --prepend /opt/homebrew/bin
+else if test -d /usr/local/bin
+    fish_add_path --global --move --prepend /usr/local/bin
+end
+
+fish_add_path --global --move --prepend $HOME/.local/bin
 
 if status is-interactive
     if command -q micromamba
