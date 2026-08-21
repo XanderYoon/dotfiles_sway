8be463cfaa93de867ea2af35c818aebd8d25cb6b
XanderYoon <alexander.ro.yoon@gmail.com>
2026-08-20 22:11:34 -0400

    Tune fish and tmux persistence settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,9 @@ end
 
 fish_add_path --global --move --prepend $HOME/.local/bin
 
+# Keep autosuggestions visibly separate from typed input.
+set --global fish_color_autosuggestion 6c7086
+
 if status is-interactive
     if command -q micromamba
         micromamba shell hook --shell fish | source
